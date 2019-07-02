777f0c6ca69ae92010e2f7ee3d722d2144f9daed
yayanyang <org.lemon@gmail.com>
2019-07-03 00:03:20 +0800

    update

---
+++ app.go
@@ -0,0 +1,95 @@
+package w3mail
+
+import (
+	"encoding/json"
+
+	"github.com/tendermint/tendermint/libs/log"
+
+	"os"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	dbm "github.com/tendermint/tendermint/libs/db"
+	tmtypes "github.com/tendermint/tendermint/types"
+)
+
+const appName = "w3mail"
+
+var (
+	// DefaultCLIHome default home directories for the application CLI
+	DefaultCLIHome = os.ExpandEnv("$HOME/.w3mailcli")
+
+	// DefaultNodeHome sets the folder where the applcation data and configuration will be stored
+	DefaultNodeHome = os.ExpandEnv("$HOME/.w3mail")
+
+	// ModuleBasics The ModuleBasicManager is in charge of setting up basic,
+	// non-dependant module elements, such as codec registration
+	// and genesis verification.
+	ModuleBasics module.BasicManager
+)
+
+func init() {
+	ModuleBasics = module.NewBasicManager()
+}
+
+// ServiceApp .
+type ServiceApp struct {
+	*baseapp.BaseApp
+	cdc *codec.Codec
+
+	keyMain *sdk.KVStoreKey
+}
+
+// MakeCodec custom tx codec
+func MakeCodec() *codec.Codec {
+	var cdc = codec.New()
+	ModuleBasics.RegisterCodec(cdc)
+	sdk.RegisterCodec(cdc)
+	codec.RegisterCrypto(cdc)
+	return cdc
+}
+
+// NewW3MailServiceApp .
+func NewW3MailServiceApp(logger log.Logger, db dbm.DB) *ServiceApp {
+
+	cdc := MakeCodec()
+
+	// BaseApp handles interactions with Tendermint through the ABCI protocol
+	bApp := baseapp.NewBaseApp(appName, logger, db, auth.DefaultTxDecoder(cdc))
+
+	app := &ServiceApp{
+		BaseApp: bApp,
+		cdc:     cdc,
+		keyMain: sdk.NewKVStoreKey(baseapp.MainStoreKey),
+	}
+
+	return app
+}
+
+// LoadHeight .
+func (app *ServiceApp) LoadHeight(height int64) error {
+	return app.LoadVersion(height, app.keyMain)
+}
+
+// ExportAppStateAndValidators .
+func (app *ServiceApp) ExportAppStateAndValidators(forZeroHeight bool, jailWhiteList []string) (appState json.RawMessage, validators []tmtypes.GenesisValidator, err error) {
+
+	// as if they could withdraw from the start of the next block
+	// ctx := app.NewContext(true, abci.Header{Height: app.LastBlockHeight()})
+
+	// if forZeroHeight {
+	// 	app.prepForZeroHeightGenesis(ctx, jailWhiteList)
+	// }
+
+	// genState := app.mm.ExportGenesis(ctx)
+	// appState, err = codec.MarshalJSONIndent(app.cdc, genState)
+	// if err != nil {
+	// 	return nil, nil, err
+	// }
+	// validators = staking.WriteValidators(ctx, app.stakingKeeper)
+	return appState, validators, nil
+}
---
+++ go.mod
@@ -0,0 +1,31 @@
+module github.com/w3hub/w3mail
+
+go 1.12
+
+require (
+	github.com/btcsuite/btcd v0.0.0-20190523000118-16327141da8c // indirect
+	github.com/cosmos/cosmos-sdk v0.28.2-0.20190626164114-c898dac6a9fc
+	github.com/cosmos/go-bip39 v0.0.0-20180819234021-555e2067c45d // indirect
+	github.com/cosmos/sdk-application-tutorial v0.0.0-20190629080119-a962f62be497
+	github.com/gorilla/mux v1.7.2 // indirect
+	github.com/magiconair/properties v1.8.1 // indirect
+	github.com/mattn/go-isatty v0.0.8 // indirect
+	github.com/onsi/ginkgo v1.8.0 // indirect
+	github.com/onsi/gomega v1.5.0 // indirect
+	github.com/pelletier/go-toml v1.4.0 // indirect
+	github.com/prometheus/common v0.4.1 // indirect
+	github.com/prometheus/procfs v0.0.0-20190523193104-a7aeb8df3389 // indirect
+	github.com/rakyll/statik v0.1.6 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20181016184325-3113b8401b8a // indirect
+	github.com/spf13/cobra v0.0.5
+	github.com/spf13/viper v1.4.0 // indirect
+	github.com/tendermint/tendermint v0.32.0
+	golang.org/x/crypto v0.0.0-20190513172903-22d7a77e9e5f // indirect
+	golang.org/x/sys v0.0.0-20190527104216-9cd6430ef91e // indirect
+	golang.org/x/text v0.3.2 // indirect
+	google.golang.org/appengine v1.4.0 // indirect
+	google.golang.org/genproto v0.0.0-20190522204451-c2c4e71fbf69 // indirect
+
+)
+
+replace golang.org/x/crypto => github.com/tendermint/crypto v0.0.0-20180820045704-3764759f34a5
